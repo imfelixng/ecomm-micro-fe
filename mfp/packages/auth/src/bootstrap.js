645553b55072f9b0f9756c0428bcdd847056f5fc
Nguyễn Quang An <ngquangan@gmail.com>
2020-12-01 18:55:11 +0700

    Create auth app and fix public path

---
+++ mfp/packages/auth/src/bootstrap.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './app';
+
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate); // Listen route changed from container
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_auth--dev-root');
+    if (devRoot) {
+        // defaultHistory: Make change url when route for auth app in dev mode
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+export {
+    mount
+}
